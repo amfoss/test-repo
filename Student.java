9109b593fb5fb7bcd5d31a7000a1ac08c80c86cd
RebelsterPi <pro.praneshulleri615@gmail.com>
2018-09-06 18:51:58 +0530

    adding the Students class
    adding driver class
    
    adding consumers

---
+++ Student.java
@@ -0,0 +1,43 @@
+package piTest;
+import java.util.*;
+public class Student {
+private String name;
+private String rollNumber;
+private String favoriteFruit;
+private float Height, Weight;
+
+void name(String s){
+	this.name=name;
+}
+void rn(String rollno) {
+	this.rollNumber=rollno;
+}
+void favoriteFruit(String fruit) {
+	this.favoriteFruit=fruit;
+}
+void Height(float height)
+{this.Height=height;}
+void Weight(float weight) {
+	this.Weight=weight;
+}
+float Weight() {
+	return this.Weight;
+}
+float Height() {
+	return this.Height;
+}
+String favoriteFruit() {
+	return this.favoriteFruit;
+}
+String name() {
+	return this.name;
+}
+String rollNumber() {
+	return this.rollNumber;
+}
+void Students(String name,String rollNumber,String favoriteFruit,float Height,float Weight){
+	
+}
+void Students() {}
+
+}
