744f3c6ae9d840166b7e4b1ca5dc70c627a4fb08
AbhilashG97 <abhilash.ag1997@gmail.com>
2018-12-12 14:48:45 +0530

    added the fruit juices

---
+++ FruitJuice/Jasna/mango.java
@@ -0,0 +1,5 @@
+public class mango {
+    public static void main(String[] args) {
+        System.out.println("Mango");
+    }
+}
---
+++ FruitJuice/Jasna/orange.java
@@ -0,0 +1,5 @@
+public class orange {
+    public static void main(String[] args) {
+        System.out.println("Orange");
+    }
+}
