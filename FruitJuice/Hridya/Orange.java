c7dc234de4d444f93bb78da45cab587550edbe2c
hridya <hridyakrishnar127@gmail.com>
2018-12-12 14:59:02 +0530

    Fruit Juices are added

---
+++ FruitJuice/Hridya/Orange.java
@@ -0,0 +1,5 @@
+public class Orange{
+  public static void main(String[] args){
+    System.out.println("Orange Juice!!");
+  }
+}
---
+++ FruitJuice/Hridya/Peach.java
@@ -0,0 +1,5 @@
+public class Peach {
+  public static void main(String[] args){
+    System.out.println("Peach juice!!");
+  }
+}
