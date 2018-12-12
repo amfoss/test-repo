814002a9aae702f2217ffa25053b04397d055d6a
athiranair2000 <athiranair2000@athinair@gmail.com>
2018-12-12 15:09:08 +0530

    Friut Juice is prepared

---
+++ FruitJuice/Athira/Mango.java
@@ -0,0 +1,5 @@
+public class Mango {
+    public static void main(String[] args) {
+        System.out.println("Mango");
+    }
+}
---
+++ FruitJuice/Athira/Starfruit.java
@@ -0,0 +1,5 @@
+public class Starfruit {
+    public static void main(String[] args) {
+        System.out.println("StarFruit");
+    }
+}
