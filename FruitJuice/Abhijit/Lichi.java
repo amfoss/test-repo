9ffa7296bee525f4b55b4db1c048ecced97bc004
abhijitramesh <abhijitramesh2k@gmail.com>
2018-12-12 15:04:28 +0530

    mixed fruit juice

---
+++ FruitJuice/Abhijit/Lichi.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author abhijit
+ *
+ */
+public class Lichi {
+
+	public static void main(String[] args) {
+		System.out.println("Lichi");
+
+	}
+
+}
---
+++ FruitJuice/Abhijit/Papaya.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author abhijit
+ *
+ */
+public class Papaya {
+
+	public static void main(String[] args) {
+		System.out.println("papaya");
+
+	}
+
+}
